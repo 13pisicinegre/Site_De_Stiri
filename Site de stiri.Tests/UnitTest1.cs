0a8f8897f95b3b9e77b889616cd3a27f0277b133
13pisicinegre <pal.paulaalex@gmail.com>
2016-11-07 14:55:03 +0200

    Add project files.

---
+++ Site de stiri.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Site_de_stiri.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
